dd89faa533edd7b5c9302d814340fd7dd4763de7
João Victor Briganti <jvbriganti@gmail.com>
2026-05-30 21:24:44 -0300

    refac: Error benchmark support float and double

--- bench/error/cosh.cpp
+++ bench/error/cosh.cpp
@@ -1,49 +1,6 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-
-#include "../rand.h"
-#include "fastmath.h"
-
-struct BenchStats {
-  double x;
-  double common;
-  double fast;
-  double abs_err;
-  double rel_err;
-};
-
-constexpr int N = 1000;
-std::vector<BenchStats> benchVec;
-
-void bench_cosh() {
-  std::vector<double> inputs = gen_random_real<double>(N);
-  benchVec.reserve(N);
-  for (int i = 0; i < N; i++) {
-    BenchStats stats;
-    stats.x = inputs[i];
-    stats.common = std::acos(stats.x);
-    stats.fast = fast_acos(stats.x);
-    stats.abs_err = std::fabs(stats.common - stats.fast);
-    stats.rel_err = stats.abs_err / std::fabs(stats.common);
-    benchVec.push_back(stats);
-  }
-}
+#include "error_bench.h"
 
 int main() {
-  bench_cosh();
-
-  FILE *output = std::fopen("output/cosh_error.csv", "w");
-  if (!output) {
-    std::perror("Failed to open output file");
-    return -1;
-  }
-
-  fprintf(output, "x,common,fast,abs_err,rel_err\n");
-  for (auto stat : benchVec) {
-    fprintf(output, "%.10g,%.15g,%.15g,%.15g,%.15g\n", stat.x, stat.common,
-            stat.fast, stat.abs_err, stat.rel_err);
-  }
-
-  return 0;
+  RUN_BENCH_1ARG(cosh);
+  return EXIT_SUCCESS;
 }
--- bench/error/pow.cpp
+++ bench/error/pow.cpp
@@ -1,65 +1,6 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-
-#include "../rand.h"
-#include "fastmath.h"
-
-struct BenchStats {
-  double a;
-  double b;
-  double common;
-  double fast;
-  double abs_err;
-  double rel_err;
-};
-
-struct PowInput {
-  double a;
-  double b;
-};
-
-constexpr int N = 1000;
-std::vector<BenchStats> benchVec;
-
-static void bench_pow() {
-  benchVec.reserve(N);
-
-  std::vector<double> inputA = gen_random_real<double>(N);
-  std::vector<double> inputB = gen_random_real<double>(N);
-  std::vector<PowInput> inputs;
-  inputs.reserve(N);
-
-  for (size_t i = 0; i < N; i++) {
-    inputs.push_back({inputA[i], inputB[i]});
-  }
-
-  for (int i = 0; i < N; i++) {
-    BenchStats stats;
-    stats.a = inputs[i].a;
-    stats.b = inputs[i].b;
-    stats.common = std::pow(stats.a, stats.b);
-    stats.fast = fast_pow(stats.a, stats.b);
-    stats.abs_err = std::fabs(stats.common - stats.fast);
-    stats.rel_err = stats.abs_err / std::fabs(stats.common);
-    benchVec.push_back(stats);
-  }
-}
+#include "error_bench.h"
 
 int main() {
-  bench_pow();
-
-  FILE *output = std::fopen("output/pow_error.csv", "w");
-  if (!output) {
-    std::perror("Failed to open output file");
-    return -1;
-  }
-
-  fprintf(output, "a,b,common,fast,abs_err,rel_err\n");
-  for (const auto &stat : benchVec) {
-    fprintf(output, "%.9g,%.9g,%.15g,%.15g,%.15g,%.15g\n", stat.a, stat.b,
-            stat.common, stat.fast, stat.abs_err, stat.rel_err);
-  }
-
-  return 0;
+  RUN_BENCH_2ARG(pow);
+  return EXIT_SUCCESS;
 }
